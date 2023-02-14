2d83dd94b69f6c4f18cde30e3afa465301a0d5d7
CBO <quddhr9523@naver.com>
2023-02-14 15:03:07 +0900

    feat: responseDTO에 aboutRate필드추가

--- src/main/java/com/example/finance7/product/dto/detail/SubscriptionDetailResponseDTO.java
+++ src/main/java/com/example/finance7/product/dto/detail/SubscriptionDetailResponseDTO.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 public class SubscriptionDetailResponseDTO extends ProductDetailResponseDTO{
 
     private String highRate;
+    private String aboutRate;
     private Integer limit;
     private String purchase;
     private String qualification;
--- src/main/java/com/example/finance7/product/dto/list/subscriptionResponseDTO.java
+++ src/main/java/com/example/finance7/product/dto/list/subscriptionResponseDTO.java
@@ -12,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 public class subscriptionResponseDTO extends ProductResponseDTO {
 
     private String highRate;
+    private String aboutRate;
     private Integer limit;
     private String purchase;
     private String qualification;
