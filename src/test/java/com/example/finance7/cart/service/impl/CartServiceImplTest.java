9b189dcf24808e711435c95b01f70d2fe642c1e5
cocoiscat <demy176@naver.com>
2023-02-15 01:44:16 +0900

    test: 회원 아이디 없이 테스트 가능하게 변경

--- src/test/java/com/example/finance7/cart/service/impl/CartServiceImplTest.java
+++ src/test/java/com/example/finance7/cart/service/impl/CartServiceImplTest.java
@@ -20,26 +20,19 @@ class CartServiceImplTest {
     CartRepository cartRepository;
 
     @Test
-    @Transactional
+
     void 장바구니_상품_추가() {
         //given
-        Long memberId1 = 1L;
         Long productId1 = 2L;
-
-        Long memberId2 = 1L;
         Long productId2 = 1L;
-
-        Long memberId3 = 2L;
         Long productId3 = 3L;
-
-        Long memberId4 = 2L;
         Long productId4 = 4L;
 
         //when
-        cartService.addCart(productId1, memberId1);
-        cartService.addCart(productId2, memberId2);
-        cartService.addCart(productId3, memberId3);
-        cartService.addCart(productId4, memberId4);
+        cartService.addCart(productId1);
+        cartService.addCart(productId2);
+        cartService.addCart(productId3);
+        cartService.addCart(productId4);
 
         //then
         Assertions.assertThat(cartRepository.count()).isEqualTo(4);
@@ -56,8 +49,8 @@ class CartServiceImplTest {
         Long productId2 = 1L;
 
 
-        cartService.addCart(memberId1, productId1);
-        SimpleVO simpleVO = cartService.addCart(memberId1, productId1);
+        cartService.addCart(productId1);
+        SimpleVO simpleVO = cartService.addCart(productId1);
 
         Assertions.assertThat(simpleVO.getStatus().contains("success")).isTrue();
     }
