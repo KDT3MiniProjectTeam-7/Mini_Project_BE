6cca53e57a53b37ebc9b24364003c039b0e0c878
cocoiscat <demy176@naver.com>
2023-02-15 01:35:28 +0900

    feat: 장바구니 조회 API Controller 설계

--- src/main/java/com/example/finance7/cart/controller/CartController.java
+++ src/main/java/com/example/finance7/cart/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.example.finance7.cart.controller;
 
 import com.example.finance7.cart.service.CartService;
+import com.example.finance7.cart.vo.CartVO;
 import com.example.finance7.cart.vo.SimpleVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,10 @@ public class CartController {
     @PostMapping("/cart")
     public SimpleVO addCart(@RequestBody Long productId, Long memberId) {
         return cartService.addCart(productId, memberId);
+    }
+
+    @GetMapping("/cart")
+    public CartVO selectAllCartProducts(@RequestBody Long productId) {
+        return cartService.selectAllCartProducts(productId);
     }
 }
