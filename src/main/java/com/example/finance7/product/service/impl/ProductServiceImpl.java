f51ef9feffc06a53f9de8ff8dee4a80b2daf6880
CBO <quddhr9523@naver.com>
2023-02-14 16:03:50 +0900

    feat: 상품 상세조회 기능 개발

--- src/main/java/com/example/finance7/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/finance7/product/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.finance7.product.service.impl;
 
-import com.example.finance7.product.dto.detail.ProductDetailResponseDTO;
+import com.example.finance7.product.dto.detail.*;
+import com.example.finance7.product.entity.*;
 import com.example.finance7.product.repository.ProductRepository;
 import com.example.finance7.product.service.ProductService;
 import com.example.finance7.product.vo.ProductResponseVO;
@@ -10,7 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,14 +23,70 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
     /**
-     * 상세 상품 조회
+     * productId를 이용하여 상세 상품 조회
      * @param productId
      * @return
      */
     @Override
     public ProductDetailResponseDTO detail(Long productId) {
 
-        return null;
+        Product product = productRepository.findById(productId).orElseThrow(
+                ()-> new NoSuchElementException("존재하지 않는 상품입니다.")
+        );
+
+        if (product instanceof Card){
+            return CardDetailResponseDTO.builder()
+                    .category("CARD")
+                    .productName(product.getProductName())
+                    .companyName(product.getCompanyName())
+                    .companyImage(product.getCompanyImage())
+                    .productURL(product.getProductURL())
+                    .benefits(((Card) product).getBenefits().split("\n"))
+                    .annualFee(((Card) product).getAnnualFee())
+                    .build();
+
+        } else if (product instanceof Loan) {
+            return LoanDetailResponseDTO.builder()
+                    .category("LOAN")
+                    .productName(product.getProductName())
+                    .companyName(product.getCompanyName())
+                    .companyImage(product.getCompanyImage())
+                    .productURL(product.getProductURL())
+                    .lowRate(((Loan) product).getLowRate())
+                    .highRate(((Loan) product).getHighRate())
+                    .bound(((Loan) product).getBound())
+                    .qualification(((Loan) product).getQualification())
+                    .build();
+
+        } else if (product instanceof Savings) {
+
+            return SavingDetailResponseDTO.builder()
+                    .category("SAVINGS")
+                    .productName(product.getProductName())
+                    .companyName(product.getCompanyName())
+                    .companyImage(product.getCompanyImage())
+                    .productURL(product.getProductURL())
+                    .basicRate(((Savings) product).getBasicRate())
+                    .primeRate(((Savings) product).getPrimeRate())
+                    .qualification(((Savings) product).getQualification())
+                    .aboutPrimeRate(((Savings) product).getAboutPrimeRate())
+                    .build();
+
+        }else {
+            Subscription subscription = (Subscription) product;
+            return SubscriptionDetailResponseDTO.builder()
+                    .category("SUBSCRIPTION")
+                    .productName(product.getProductName())
+                    .companyName(product.getCompanyName())
+                    .companyImage(product.getCompanyImage())
+                    .productURL(product.getProductURL())
+                    .highRate(subscription.getHighRate())
+                    .aboutRate(subscription.getAboutRate())
+                    .bound(subscription.getBound())
+                    .purchase(subscription.getPurchase())
+                    .qualification(subscription.getQualification())
+                    .build();
+        }
     }
 
     /**
