0844b8f55edf97364a1eec9bdd261389ad434875
ykim-yeji <ykim5867@gmail.com>
2023-02-15 22:12:47 +0900

    feat: 상태 정보 response dto 추가

---
+++ src/main/java/com/example/finance7/member/dto/StatusResponse.java
@@ -0,0 +1,14 @@
+package com.example.finance7.member.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class StatusResponse {
+
+    protected String status;
+}
