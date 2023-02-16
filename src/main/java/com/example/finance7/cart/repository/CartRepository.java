a59a552b2f07f80e4dfc58fdc7379d258d37235b
cocoiscat <demy176@naver.com>
2023-02-16 15:28:47 +0900

    feat: 멤버와 상품으로 장바구니 요소 삭제

--- src/main/java/com/example/finance7/cart/repository/CartRepository.java
+++ src/main/java/com/example/finance7/cart/repository/CartRepository.java
@@ -14,4 +14,6 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @EntityGraph(attributePaths = {"product"})
     List<Cart> findCartsByMember(Member member);
+
+    void deleteCartByMemberAndProduct(Member member, Product product);
 }
