f19a0cd06360ac50901d4636a125d3c7af64e157
ykim-yeji <ykim5867@gmail.com>
2023-02-16 14:38:53 +0900

    refactor: MemberInfoService의 메소드 선언부 수정 및 추가

--- src/main/java/com/example/finance7/member/service/MemberInfoService.java
+++ src/main/java/com/example/finance7/member/service/MemberInfoService.java
@@ -1,7 +1,7 @@
 package com.example.finance7.member.service;
 
-import com.example.finance7.member.dto.SomeMemberInfoDto;
 import com.example.finance7.member.dto.SomeMemberUpdateInfoDto;
+import com.example.finance7.member.dto.StatusResponse;
 import com.example.finance7.member.entity.Member;
 
 import java.util.Date;
@@ -9,9 +9,11 @@ import java.util.Date;
 public interface MemberInfoService {
     Member findAllMemberInfo();
 
-    SomeMemberInfoDto findSomeMemberInfo();
+    StatusResponse findSomeMemberInfo();
 
-    String updateSomeMemberInfo(SomeMemberUpdateInfoDto requestDto);
+    StatusResponse updateSomeMemberInfo(SomeMemberUpdateInfoDto requestDto);
 
     int calculateAge(Date birthday);
+
+    StatusResponse makeStatusResponse(String status);
 }
