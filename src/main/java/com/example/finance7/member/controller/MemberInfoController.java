f2be44815cfba521f17e88c0d0c1faf94c59ed59
ykim-yeji <ykim5867@gmail.com>
2023-02-16 14:46:18 +0900

    refactor: MemberInfoController 메소드 수정
    - 일부 회원정보 조회와 수정 메소드 모두 service로부터 상태 정보 response dto를 받아서 반환하도록 수정

--- src/main/java/com/example/finance7/member/controller/MemberInfoController.java
+++ src/main/java/com/example/finance7/member/controller/MemberInfoController.java
@@ -1,15 +1,11 @@
 package com.example.finance7.member.controller;
 
-import com.example.finance7.member.dto.SomeMemberInfoDto;
 import com.example.finance7.member.dto.SomeMemberUpdateInfoRequest;
 import com.example.finance7.member.dto.StatusResponse;
 import com.example.finance7.member.service.MemberInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.NoSuchElementException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,17 +22,7 @@ public class MemberInfoController {
      */
     @GetMapping("user")
     public StatusResponse findSomeMemberInfo() {
-        try {
-            SomeMemberInfoDto memberInfoDto = memberInfoService.findSomeMemberInfo();
-
-            return memberInfoDto.toResponse("success");
-        } catch (NullPointerException | NoSuchElementException e) {
-            log.error(e.getMessage());
-
-            return StatusResponse.builder()
-                    .status("fail")
-                    .build();
-        }
+        return memberInfoService.findSomeMemberInfo();
     }
 
     /**
@@ -47,10 +33,6 @@ public class MemberInfoController {
      */
     @PatchMapping("user")
     public StatusResponse updateSomeMemberInfo(@RequestBody SomeMemberUpdateInfoRequest memberUpdateInfoRequest) {
-        String status = memberInfoService.updateSomeMemberInfo(memberUpdateInfoRequest.toDto());
-
-        return StatusResponse.builder()
-                .status(status)
-                .build();
+        return memberInfoService.updateSomeMemberInfo(memberUpdateInfoRequest.toDto());
     }
 }
