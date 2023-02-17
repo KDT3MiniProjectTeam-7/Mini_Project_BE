69b387f3dbd47f29b3a1488d50e6b930feddf197
ykim-yeji <ykim5867@gmail.com>
2023-02-18 06:15:11 +0900

    fix: 회원 정보 ServiceImpl 전반적 수정
    - javadoc 내용 수정
    - DTO 이름 변경 사항 적용

--- src/main/java/com/example/finance7/member/service/impl/MemberInfoServiceImpl.java
+++ src/main/java/com/example/finance7/member/service/impl/MemberInfoServiceImpl.java
@@ -1,19 +1,19 @@
 package com.example.finance7.member.service.impl;
 
+import com.example.finance7.config.JwtProvider;
+import com.example.finance7.member.dto.MemberInfoUpdateRequestDTO;
+import com.example.finance7.member.dto.MemberRequestDTO;
 import com.example.finance7.member.dto.MemberSearchHistoryResponseDTO;
-import com.example.finance7.member.dto.SomeMemberUpdateInfoDto;
-import com.example.finance7.member.dto.StatusResponse;
+import com.example.finance7.member.dto.StatusResponseDTO;
 import com.example.finance7.member.entity.Member;
 import com.example.finance7.member.entity.Scession;
 import com.example.finance7.member.entity.SearchHistory;
 import com.example.finance7.member.repository.MemberRepository;
 import com.example.finance7.member.repository.SearchHistoryRepository;
 import com.example.finance7.member.service.MemberInfoService;
-import com.example.finance7.member.service.MemberService;
 import com.example.finance7.member.vo.MemberSearchHistoryResponseVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -26,17 +26,17 @@ public class MemberInfoServiceImpl implements MemberInfoService {
     private final MemberRepository memberRepository;
     private final MemberServiceImpl memberService;
     private final SearchHistoryRepository searchHistoryRepository;
+    private final JwtProvider jwtProvider;
 
     /**
-     *전체 회원정보를 조회한다.
+     * 회원 조회
      *
-     * @return 현재 로그인 중인 member 전체 정보 반환 (Entity)
+     * @return Member 회원 반환 (Entity)
      */
     @Override
-    public Member findAllMemberInfo() throws NullPointerException, NoSuchElementException {
-        String email = String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new NullPointerException("존재하지 않는 회원입니다."));
+    public Member findMember(String header) throws NullPointerException {
+        MemberRequestDTO memberRequestDTO = new MemberRequestDTO(jwtProvider.tokenToMember(header));
+        Member member = memberService.findMemberByEmail(memberRequestDTO.getEmail());
 
         if (!memberService.isOpenUser(member)) {
             throw new NoSuchElementException("이미 탈퇴한 회원입니다.");
@@ -45,17 +45,17 @@ public class MemberInfoServiceImpl implements MemberInfoService {
     }
 
     /**
-     * 일부 회원정보를 조회한다.
+     * 회원 정보 조회
      *
-     * @return 현재 로그인 중인 member의 email, name, age, tags 정보 반환 (dto)
+     * @return email, name, age, tags 정보와 status 정보 반환 (response dto)
      */
     @Override
-    public StatusResponse findSomeMemberInfo() {
+    public StatusResponseDTO findMemberInfo(String header) {
         try {
-            Member member = findAllMemberInfo();
+            Member member = findMember(header);
             int age = calculateAge(member.getBirthDay());
 
-            return member.toSomeMemberInfoResponse("success", age);
+            return member.toMemberInfoDTO("success", age);
         } catch (NullPointerException | NoSuchElementException e) {
             log.error(e.getMessage());
 
@@ -65,13 +65,13 @@ public class MemberInfoServiceImpl implements MemberInfoService {
     }
 
     /**
-     * 회원정보를 수정한다.
+     * 회원 정보 수정
      *
-     * @param memberUpdateInfoDto 변경할 회원정보 값 (dto)
-     * @return 회원정보 수정 성공 시 success, 실패 시 fail 반환 (String)
+     * @param memberUpdateInfoDto 변경할 회원 정보 값 (request dto)
+     * @return 회원 정보 수정 성공 시 success, 실패 시 fail 반환 (String)
      */
     @Override
-    public StatusResponse updateSomeMemberInfo(SomeMemberUpdateInfoDto memberUpdateInfoDto) {
+    public StatusResponseDTO updateMemberInfo(MemberInfoUpdateRequestDTO memberUpdateInfoDto, String header) {
         try {
             Member responseMember = findAllMemberInfo();
             Member requestMember = memberUpdateInfoDto.toEntityWithUpdate(responseMember);
@@ -82,16 +82,17 @@ public class MemberInfoServiceImpl implements MemberInfoService {
 
             memberUpdateInfoDto.setNewPassword(memberService.encodePassword(memberUpdateInfoDto.getNewPassword()));
             memberRepository.save(memberUpdateInfoDto.toEntityWithUpdate(responseMember));
+
+            return makeStatusResponse("success");
         } catch (NullPointerException | NoSuchElementException e) {
             log.error(e.getMessage());
 
             return makeStatusResponse("fail");
         }
-        return makeStatusResponse("success");
     }
 
     /**
-     * 나이를 계산한다.
+     * 생일로 나이 계산
      *
      * @param birthday 생일날짜시간 (Date)
      * @return 나이 (int)
@@ -103,23 +104,21 @@ public class MemberInfoServiceImpl implements MemberInfoService {
 
         if (today.get(Calendar.MONTH) > birthday.getMonth()) {
             return age;
-        }
-
-        if (today.get(Calendar.MONTH) == birthday.getMonth() && today.get(Calendar.DATE) >= birthday.getDate()) {
+        } else if (today.get(Calendar.MONTH) == birthday.getMonth() && today.get(Calendar.DATE) >= birthday.getDate()) {
             return age;
         }
         return age - 1;
     }
 
     /**
-     * 상태 코드 response dto 생성
+     * status 정보 response dto 생성
      *
      * @param status 상태 (String)
-     * @return 상태 정보 반환 (response dto)
+     * @return status 정보 반환 (response dto)
      */
     @Override
-    public StatusResponse makeStatusResponse(String status) {
-        return StatusResponse.builder()
+    public StatusResponseDTO makeStatusResponse(String status) {
+        return StatusResponseDTO.builder()
                 .status(status)
                 .build();
     }
