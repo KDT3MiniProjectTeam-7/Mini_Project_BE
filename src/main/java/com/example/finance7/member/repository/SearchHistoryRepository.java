5616798c8a765b6422f215e41944850acb70fffd
cocoiscat <demy176@naver.com>
2023-02-17 15:55:38 +0900

    feat: SearchHistory Repository 구현

---
+++ src/main/java/com/example/finance7/member/repository/SearchHistoryRepository.java
@@ -0,0 +1,20 @@
+package com.example.finance7.member.repository;
+
+import com.example.finance7.member.entity.SearchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
+
+    boolean existsByMemberIdAndSearchContent(Long memberId, String searchContent);
+
+    int countByMemberId(Long memberId);
+
+    @Query("SELECT min(s.historyId) FROM SearchHistory s where s.memberId = :memberId")
+    Long findOldestElement(@Param("memberId") Long memberId);
+    @Modifying
+    @Query("delete from SearchHistory s where s.historyId = :historyId")
+    void deleteOldestElement(@Param("historyId") Long historyId);
+}
