6749e82228bd0d607dd04f311415414044fefbbe
ykim-yeji <ykim5867@gmail.com>
2023-02-21 17:39:32 +0900

    remove: 회원 조회 메소드 삭제

--- src/main/java/com/example/finance7/member/service/MemberInfoService.java
+++ src/main/java/com/example/finance7/member/service/MemberInfoService.java
@@ -3,13 +3,11 @@ package com.example.finance7.member.service;
 import com.example.finance7.member.dto.MemberInfoUpdateRequestDTO;
 import com.example.finance7.member.dto.StatusResponseDTO;
 import com.example.finance7.member.dto.DeleteAllResponseDTO;
-import com.example.finance7.member.entity.Member;
 import com.example.finance7.member.vo.MemberSearchHistoryResponseVO;
 
 import java.util.Date;
 
 public interface MemberInfoService {
-    Member findMember(String header);
 
     StatusResponseDTO findMemberInfo(String header);
 
